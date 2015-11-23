067829be8f783fb00d6eda7e3e9c5cf1dea2a568
Johannes Reuter <jreuter@inovex.de>
2015-11-23 16:35:11 +0100

    Updated example according to https://github.com/dart-lang/polymer-dart/issues/649

--- test/my_component_test.dart
+++ test/my_component_test.dart
@@ -1,10 +1,9 @@
 @TestOn("browser")
 import "dart:html";
-import "dart:async";
 import "package:test/test.dart";
 import 'package:polymer/polymer.dart';
 
-import '../web/my_component.dart';
+import 'package:polymer_dart_example/my_component.dart';
 
 main() async {
   MyComponent componentUnderTest;
@@ -12,14 +11,11 @@ main() async {
   await initPolymer();
   
   setUp(() {
-    //document.body.children.clear();
-    //componentUnderTest = new Element.tag('my-component');
-    //document.body.append(componentUnderTest);
     componentUnderTest = document.body.querySelector("#abc");
   });
   
   test("my-component should be initialized correctly", () {
-    Element heading = componentUnderTest.shadowRoot.querySelector('h1');
+    Element heading = new PolymerDom(componentUnderTest.root).querySelector('h1');
     expect(heading.text, equals('This is my component'));
   });
 }
